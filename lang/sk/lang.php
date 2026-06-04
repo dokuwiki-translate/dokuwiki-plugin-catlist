fa6035c909b4030520a8786de058a8a69b21f8b7
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:26:05 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['unknownoption']         = 'Neznáma voľba %s pre &lt;catlist&gt;';
+$lang['outofpages']            = 'Musíte zostať v priečinku <code>data/pages</code>!';
+$lang['dontexist']             = 'Menný priestor <code>%s</code> neexistuje';
+$lang['addpage']               = 'Pridať stránku...';
---
+++ lang/sk/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['default_sort']          = 'Predvolené zoradenie';
+$lang['hide_index']            = 'Skryť stránky používané ako indexové stránky (hlavná stránka menného priestoru)';
+$lang['index_priority']        = 'Poradie priority stránok používaných ako indexové stránky, medzi start, inside a outside. Príklad s menným priestorom foo:bar: start= foo:bar:&lt;start&gt;; outside= foo:bar ; inside= foo:bar:bar';
+$lang['nocache']               = 'Vypnúť cache stránok, kde sa používa catlist';
+$lang['hide_acl_nsnotr']       = 'Skryť menné priestory, na ktoré používateľ nemá ACL právo na čítanie';
+$lang['show_acl']              = 'Ignorovať ACL (zobraziť všetko bez ohľadu na používateľa) a zobraziť oprávnenia používateľa pre každú položku';
+$lang['useheading']            = 'Použiť prvý nadpis ako názov stránky (bez ohľadu na nastavenie \'useheading\' v DokuWiki)';
+$lang['showhead']              = 'Predvolene zobrazovať nadpis; individuálne prepísanie pomocou -noHead a -showHead.';
+$lang['show_leading_ns']       = 'Zobraziť nadradené menné priestory stránky, na ktorú má používateľ ACL právo na čítanie, bez ohľadu na ACL menného priestoru.';
+$lang['nswildcards']           = 'Nahradiť zástupné znaky v reťazci menného priestoru ako pri šablónach stránok.';
+$lang['pagename_sanitize']     = 'Sanitizovať názov stránok vytvorených tlačidlom \'Pridať stránku\'. Keď je táto voľba zapnutá, funkcia rešpektuje konfiguračné voľby \'deaccent\', \'userewrite\', \'useslash\' a \'sepchar\'.';
+$lang['sort_collator_locale']  = 'Locale použité na zoraďovanie názvov stránok. Ak nie je prázdne, použije sa PHP trieda Collator; musí to byť platný krátky kód locale. Ak je prázdne, použije sa obyčajné alfanumerické zoraďovanie.';
